7992e159aca4535438388b5633a26ea109567b69
praveenbesetti <praveenbesetti0@gmail.com>
2025-12-10 20:32:59 +0530

    deployment

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = "https://backend-deployment.duckdns.org /api";
 
 export const getS3Url = () => axios.get(`${API}/s3-url`);
 export const registerUser = (data) => axios.post(`${API}/register`, data);
