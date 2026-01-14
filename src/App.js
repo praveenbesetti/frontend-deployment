5d751add0ec30410d605ddd4dae699e467faf0d4
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-01-14 18:48:07 +0530

    Add console log for S3 upload URL data
    Signed-off-by: praveen <135096397+praveenbesetti@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
     
   // pass the file type to backend
   const { data } = await getS3Url(form.image.type);
-
+  console.log(data)
     
   // upload with the correct content-type
   await fetch(data.uploadURL, {
