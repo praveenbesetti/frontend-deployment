f35543ae6128e6e53cf649809f5cd299de5df1a9
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-01-14 18:51:12 +0530

    Log API URL in api.js
    Signed-off-by: praveen <135096397+praveenbesetti@users.noreply.github.com>

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API = "https://backend-deployment.duckdns.org/api";
-
+console.log(API)
 export const getS3Url = () => axios.get(`${API}/s3-url`);
 export const registerUser = (data) => axios.post(`${API}/register`, data);
