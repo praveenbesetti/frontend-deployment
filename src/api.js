04b67153c757260dbafbb03984e2b3a16fac9f28
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-01-14 18:55:30 +0530

    Log API URL with a prefix in api.js
    Signed-off-by: praveen <135096397+praveenbesetti@users.noreply.github.com>

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = "https://backend-deployment.duckdns.org/api";
-console.log(API)
+
+console.log("api"+API)
 export const getS3Url = () => axios.get(`${API}/s3-url`);
 export const registerUser = (data) => axios.post(`${API}/register`, data);
