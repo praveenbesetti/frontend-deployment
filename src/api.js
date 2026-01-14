2bff0775df7289b4a00e9b9f626ccd8864d9ed98
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-01-14 19:02:37 +0530

    Remove console log for API URL
    Remove console log statement for API URL.
    
    Signed-off-by: praveen <135096397+praveenbesetti@users.noreply.github.com>

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const API = "https://backend-deployment.duckdns.org/api";
 
-console.log("api"+API)
+
 export const getS3Url = () => axios.get(`${API}/s3-url`);
 export const registerUser = (data) => axios.post(`${API}/register`, data);
